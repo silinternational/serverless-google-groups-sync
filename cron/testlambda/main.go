63b3005b4bbd25763d18095901ce6631341c7269
Unknown <baggerone@gmail.com>
2019-03-18 16:43:45 -0400

    Add function to save S3 file to disk

--- cron/testlambda/main.go
+++ cron/testlambda/main.go
@@ -8,8 +8,10 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"io/ioutil"
 )
 
+const GoogleCredsJsonFile = "/tmp/google-creds.json"
 
 type TestConfig struct {
 	GroupsMapS3ARN string
@@ -22,7 +24,7 @@ func (t *TestConfig) setRequired() error {
 	errMsg := "Error: required value missing for environment variable %s"
 
 
-	envKey := "S3_BUCKET_FOR_GROUPS_MAP"
+	envKey := "S3_BUCKET_FOR_INPUT"
 	value := os.Getenv(envKey)
 	if value == "" {
 		return fmt.Errorf(errMsg, envKey)
@@ -38,6 +40,22 @@ func (t *TestConfig) setDefaults() {
 	}
 }
 
+func saveGoogleCredsJsonFile(objectOutput *s3.GetObjectOutput) error {
+	body := objectOutput.Body
+
+	bodyBuf, err := ioutil.ReadAll(body)
+	if err != nil {
+		return fmt.Errorf("Unable to read Google Credentials file from S3: ", err)
+	}
+
+	err = ioutil.WriteFile(GoogleCredsJsonFile, bodyBuf, 0644)
+	if err != nil {
+		return fmt.Errorf("Unable to write Google Credentials to disk: ", err)
+	}
+
+	// log.Println("Wrote following to disk: \n ", string(bodyBuf))
+	return nil
+}
 
 func handler(config TestConfig) error {
 	log.Println("Starting TestLambda.")
@@ -59,21 +77,25 @@ func handler(config TestConfig) error {
 
 	sess := session.Must(session.NewSession())
 
-	// Create a new instance of the service's client with a Session.
-	// Optional aws.Config values can also be provided as variadic arguments
-	// to the New function. This option allows you to provide service
-	// specific configuration.
 	svc := s3.New(sess)
 
-	object, err := svc.GetObject(&bucketItem)
+	s3Object, err := svc.GetObject(&bucketItem)
 
 	if err != nil {
 		log.Println("Unable to get Groups Map file from S3 ... ")
 		return err
 	}
 
-	log.Println("Success: S3 file length: ", fmt.Sprintf("%v", object.ContentLength))
+	// TODO Call a function that converts the Groups Map file from json into a golang map
 
+	// TODO Call this with the actual google credentials json file (not the Groups Map file)
+	err = saveGoogleCredsJsonFile(s3Object)
+
+	if err != nil {
+		return err
+	}
+
+	log.Println("Success: S3 file length: ", fmt.Sprintf("%d", &s3Object.ContentLength))
 
 	return nil
 }
