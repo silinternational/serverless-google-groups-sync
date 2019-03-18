95e254fa2205fa38d279f5bdc095755a712891f0
Unknown <baggerone@gmail.com>
2019-03-18 15:56:13 -0400

    export tier level env vars

--- run-slsdeploy.sh
+++ run-slsdeploy.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+export S3_BUCKET_FOR_GROUPS_MAP="${DEV_S3_BUCKET_FOR_GROUPS_MAP}"
+
+export VPC_SG_ID="${DEV_VPC_SG_ID}"
+export VPC_SUBNET1="${DEV_VPC_SUBNET1}"
+export VPC_SUBNET2="${DEV_VPC_SUBNET2}"
+export VPC_SUBNET3="${DEV_VPC_SUBNET3}"
+
 mkdir -p ~/.aws
 
 cp /go/src/github.com/silinternational/serverless-google-groups-sync/aws.credentials ~/.aws/credentials
